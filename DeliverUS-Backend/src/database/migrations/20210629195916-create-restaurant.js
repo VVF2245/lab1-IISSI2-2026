b1d5fb3fa1f59a1ef768a1006706f3b86016a73e
VVF2245 <luihercua@alum.us.es>
2026-02-18 19:29:53 +0100

    Update Lab1

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,8 +6,41 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
+      },
       // TODO: Include the rest of the fields of the Restaurants table
+      name: Sequelize.STRING,
+      description: {
+        type: Sequelize.String,
+        allowNull: true
+      },
+      address: Sequelize.String,
+      postalCode: Sequelize.String,
+      url: {
+        type: Sequelize.String,
+        allowNull: true
+      },
+      shippingCosts: Sequelize.Float,
+      averageServiceMinutes: {
+        type: Sequelize.Float,
+        allowNull: true
+      },
+      email: {
+        type: Sequelize.String,
+        allowNull: true
+      },
+      phone: {
+        type: Sequelize.String,
+        allowNull: true
+      },
+      logo: {
+        type: Sequelize.String,
+        allowNull: true
+      },
+      heroImage: {
+        type: Sequelize.String,
+        allowNull: true
+      },
+      status: Sequelize.Enum('online', 'offline', 'closed', 'temporarily closed')
     })
   },
   down: async (queryInterface, Sequelize) => {
